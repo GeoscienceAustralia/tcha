9dc911bec332bfa218e0b079800cb3abbcb35eee
Kieran Ricardo <kieranricardo@hotmail.com>
2021-10-19 17:31:37 +1100

    edit

--- synoptic-flow/tc_predictions.py
+++ synoptic-flow/tc_predictions.py
@@ -66,6 +66,10 @@ if __name__ == '__main__':
     uds_250s = client.compute(uds_250s)
     vds_250s = client.compute(vds_250s)
 
+    uds_850s = [x.result() for x in uds_850s]
+    vds_850s = [x.result() for x in vds_850s]
+    uds_250s = [x.result() for x in uds_250s]
+    vds_250s = [x.result() for x in vds_250s]
     out = []
     prev_eventid = None
     prev_month = None
@@ -74,68 +78,68 @@ if __name__ == '__main__':
     lons = [None]
     ids = set()
 
-    for i, row in enumerate(df.itertuples()):
-
-        if row.eventid not in ids:
-            # using forward difference
-            # discard last position of previous TC track
-            lats[-1] = row.Latitude
-            lons[-1] = row.Longitude
-            ids.add(row.eventid)
-
-        if np.isnan(lats[-1]):
-            # TC went out of domain
-            lats.append(np.nan)
-            lons.append(np.nan)
-            continue
-
-        timestamp = row.Datetime
-
-        # don't generate tracks
-        # perform a one step prediction
-        lat = row.Latitude
-        lon = row.Longitude
-
-        uds_850 = uds_850s[i].result()
-        uds_250 = uds_250s[i].result()
-        vds_850 = vds_850s[i].result()
-        vds_250 = vds_250s[i].result()
-        for _ in range(6):
-
-            try:
-                uds_interp_850 = uds_850.interp(time=timestamp, latitude=lat, longitude=lon)
-                vds_interp_850 = vds_850.interp(time=timestamp, latitude=lat, longitude=lon)
-
-                uds_interp_250 = uds_250.interp(time=timestamp, latitude=lat, longitude=lon)
-                vds_interp_250 = vds_250.interp(time=timestamp, latitude=lat, longitude=lon)
-
-                u = -3.0575 + 0.4897 * uds_interp_850 + 0.6752 * uds_interp_250
-                v = -5.1207 + 0.3257 * vds_interp_850 + 0.1502 * vds_interp_250
-
-                dt = 1  # hours
-                bearing = np.arctan(u / v) * 180 / np.pi
-                distance = np.sqrt(u ** 2 + v ** 2) * dt
-
-                origin = geopy.Point(lats[-1], lons[-1])
-                destination = geodesic(kilometers=distance).destination(origin, bearing)
-                lat = destination.latitude
-                lon = destination.longitude
-
-            except IndexError:
-                lat = np.nan
-                lon = np.nan
-
-            timestamp += np.timedelta64(1, 'h')
-
-        lats.append(lat)
-        lons.append(lon)
+    # for i, row in enumerate(df.itertuples()):
+    #
+    #     if row.eventid not in ids:
+    #         # using forward difference
+    #         # discard last position of previous TC track
+    #         lats[-1] = row.Latitude
+    #         lons[-1] = row.Longitude
+    #         ids.add(row.eventid)
+    #
+    #     if np.isnan(lats[-1]):
+    #         # TC went out of domain
+    #         lats.append(np.nan)
+    #         lons.append(np.nan)
+    #         continue
+    #
+    #     timestamp = row.Datetime
+    #
+    #     # don't generate tracks
+    #     # perform a one step prediction
+    #     lat = row.Latitude
+    #     lon = row.Longitude
+    #
+    #     uds_850 = uds_850s[i].result()
+    #     uds_250 = uds_250s[i].result()
+    #     vds_850 = vds_850s[i].result()
+    #     vds_250 = vds_250s[i].result()
+    #     for _ in range(6):
+    #
+    #         try:
+    #             uds_interp_850 = uds_850.interp(time=timestamp, latitude=lat, longitude=lon)
+    #             vds_interp_850 = vds_850.interp(time=timestamp, latitude=lat, longitude=lon)
+    #
+    #             uds_interp_250 = uds_250.interp(time=timestamp, latitude=lat, longitude=lon)
+    #             vds_interp_250 = vds_250.interp(time=timestamp, latitude=lat, longitude=lon)
+    #
+    #             u = -3.0575 + 0.4897 * uds_interp_850 + 0.6752 * uds_interp_250
+    #             v = -5.1207 + 0.3257 * vds_interp_850 + 0.1502 * vds_interp_250
+    #
+    #             dt = 1  # hours
+    #             bearing = np.arctan(u / v) * 180 / np.pi
+    #             distance = np.sqrt(u ** 2 + v ** 2) * dt
+    #
+    #             origin = geopy.Point(lats[-1], lons[-1])
+    #             destination = geodesic(kilometers=distance).destination(origin, bearing)
+    #             lat = destination.latitude
+    #             lon = destination.longitude
+    #
+    #         except IndexError:
+    #             lat = np.nan
+    #             lon = np.nan
+    #
+    #         timestamp += np.timedelta64(1, 'h')
+    #
+    #     lats.append(lat)
+    #     lons.append(lon)
 
     print(time.time() - t0, 's')
 
-    df = df.iloc[:len(lats)].copy()
-    lats = lats[:len(df)]
-    lons = lons[:len(df)]
-
-    df['lats_sim'] = np.array(lats)
-    df['lons_sim'] = np.array(lons)
-    df.to_csv(os.path.expanduser("~/tc_predictions.csv"))
+    # df = df.iloc[:len(lats)].copy()
+    # lats = lats[:len(df)]
+    # lons = lons[:len(df)]
+    #
+    # df['lats_sim'] = np.array(lats)
+    # df['lons_sim'] = np.array(lons)
+    # df.to_csv(os.path.expanduser("~/tc_predictions.csv"))
