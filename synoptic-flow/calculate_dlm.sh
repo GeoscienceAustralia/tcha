a6f1f1f63ac427ffc593eb11b0de78298f3d567c
Kieran Ricardo <kieranricardo@hotmail.com>
2021-11-08 10:45:22 +1100

    mpi era5 extract

--- synoptic-flow/calculate_dlm.sh
+++ synoptic-flow/calculate_dlm.sh
@@ -3,14 +3,13 @@
 #PBS -Pw85
 #PBS -qnormal
 #PBS -N era5dlm
-#PBS -m ae
+#PBS -m abe
 #PBS -M kieran.ricardo@ga.gov.au
 #PBS -lwalltime=1:00:00
 #PBS -lmem=128GB,ncpus=48,jobfs=4000MB
 #PBS -W umask=0002
 #PBS -joe
 #PBS -lstorage=gdata/w85+scratch/w85+gdata/rt52
-#PBS -v NJOBS,NJOB,YEAR
 
 module purge
 module load pbs
