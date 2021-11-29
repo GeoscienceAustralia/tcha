ab4399604336118d399932aab64ab62de3ae182d
Kieran Ricardo <kieranricardo@hotmail.com>
2021-11-29 14:31:18 +1100

    reduced time range

--- synoptic-flow/simulate_tc_tracks.py
+++ synoptic-flow/simulate_tc_tracks.py
@@ -106,11 +106,11 @@ month_rates = {
 }
 
 
-for year in rank_years:
+for year in rank_years[1:]:
     print(f"Loading data for {month}/{year}")
     udlm, vdlm = load_dlm(year, 1)
     print(f"Finished loading data for {month}/{year}. Time taken: {t1 - t0}s")
-    for month in range(1, 13):
+    for month in range(1, 2):
 
         t0 = time.time()
         # sufficient repeats that the sum should be equal to the mean * number of repeats
