ab69d5049555f2fe65144d25a9c1f4a9ec53f407
Kieran Ricardo <kieranricardo@hotmail.com>
2021-11-29 14:48:42 +1100

    linear model

--- synoptic-flow/simulate_tc_tracks.py
+++ synoptic-flow/simulate_tc_tracks.py
@@ -106,9 +106,9 @@ month_rates = {
 
 
 for year in rank_years[1:]:
-    print(f"Loading data for {month}/{year}")
+    print(f"Loading data for {1}/{year}")
     udlm, vdlm = load_dlm(year, 1)
-    print(f"Finished loading data for {month}/{year}. Time taken: {t1 - t0}s")
+    print(f"Finished loading data for {1}/{year}. Time taken: {t1 - t0}s")
     for month in range(1, 2):
 
         t0 = time.time()
