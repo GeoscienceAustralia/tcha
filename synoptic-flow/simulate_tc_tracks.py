722a73875eccbe9abd92c2a366d30022af97a2c7
Kieran Ricardo <kieranricardo@hotmail.com>
2021-11-29 15:41:07 +1100

    linear model

--- synoptic-flow/simulate_tc_tracks.py
+++ synoptic-flow/simulate_tc_tracks.py
@@ -26,11 +26,11 @@ def load_dlm(year, month):
     vfile = f"{prefix}/v/{year}/v_era5_oper_pl_{year}{month:02d}01-{year}{month:02d}{days}.nc"
 
     uds = xr.open_dataset(ufile, chunks='auto')
-    uenv = uds.u.sel(level=pslice, longitude=long_slice, latitude=lat_slice).compute()
+    uenv = uds.u.sel(level=pslice, longitude=long_slice, latitude=lat_slice).compute(scheduler='single-threaded')
     udlm = np.trapz(uenv.data, pressure, axis=1) / 550
 
     vds = xr.open_dataset(vfile, chunks='auto')
-    venv = vds.v.sel(level=pslice, longitude=long_slice, latitude=lat_slice).compute()
+    venv = vds.v.sel(level=pslice, longitude=long_slice, latitude=lat_slice).compute(scheduler='single-threaded')
     vdlm = np.trapz(venv.data, pressure, axis=1) / 550
 
     udlm = xr.DataArray(
