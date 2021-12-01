26bb1aa4c19fa625769a3be540145851710575b1
Kieran Ricardo <kieranricardo@hotmail.com>
2021-12-01 12:04:58 +1100

    bump everything up

--- synoptic-flow/era5_dlm.sh
+++ synoptic-flow/era5_dlm.sh
@@ -6,7 +6,7 @@
 #PBS -m abe
 #PBS -M kieran.ricardo@ga.gov.au
 #PBS -l walltime=0:20:00
-#PBS -l mem=128GB,ncpus=48,jobfs=4000MB
+#PBS -l mem=192GB,ncpus=48,jobfs=4000MB
 #PBS -W umask=0002
 #PBS -joe
 #PBS -lstorage=gdata/w85+scratch/w85+gdata/rt52+gdata/v10+gdata/fj6
@@ -17,4 +17,4 @@ module load dea/20210527
 
 cd $HOME/tcha/synoptic-flow
 
-mpiexec -n 4 python3 era5_dlm.py
+mpiexec -n 6 python3 era5_dlm.py
