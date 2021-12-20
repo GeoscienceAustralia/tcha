4a48e67c50e640c04db72fbbdb1d106c63e89d4a
Kieran Ricardo <kieranricardo@hotmail.com>
2021-12-20 15:30:32 +1100

    edits

--- synoptic-flow/analyse_tc_tracks.sh
+++ synoptic-flow/analyse_tc_tracks.sh
@@ -6,7 +6,7 @@
 #PBS -m abe
 #PBS -M kieran.ricardo@ga.gov.au
 #PBS -l walltime=5:00:00
-#PBS -l mem=20GB,ncpus=2, jobfs=4000MB
+#PBS -l mem=20GB,ncpus=2,jobfs=4000MB
 #PBS -W umask=0002
 #PBS -joe
 #PBS -lstorage=gdata/w85+scratch/w85+gdata/rt52+gdata/v10+gdata/fj6
