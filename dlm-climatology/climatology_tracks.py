ab4a25e2b0109766642db19570640c885e16a6dc
Kieran Ricardo <kieranricardo@hotmail.com>
2021-12-21 16:51:32 +1100

    edits

--- dlm-climatology/climatology_tracks.py
+++ dlm-climatology/climatology_tracks.py
@@ -64,11 +64,16 @@ def perturbation(t, N, T, phase):
     return a * (n_inv_23 * np.sin(2 * np.pi * (phase + n * t[:, None] / T))).sum(axis=1)
 
 
-def tc_velocity(climatology, t, N, T, phase, idxs):
+def tc_velocity(climatologies, t, N, T, phase, idxs1, idxs2, weights1, weights2):
     pert = perturbation(t, N, T, phase)
 
-    u = climatology[0].take(idxs) + climatology[1].take(idxs) * pert
-    v = climatology[2].take(idxs) + climatology[3].take(idxs) * pert
+    u1 = climatologies[0].take(idxs1) + climatologies[1].take(idxs1)
+    v1 = climatologies[2].take(idxs1) + climatologies[3].take(idxs1)
+    u2 = climatologies[0].take(idxs2) + climatologies[1].take(idxs2)
+    v2 = climatologies[2].take(idxs2) + climatologies[3].take(idxs2)
+
+    u = (u1 * weights1 + u2 * weights2) / (weights1 + weights2)
+    v = (v1 * weights1 + v2 * weights2) / (weights1 + weights2)
 
     u = -4.5205 + 0.8978 * u * 3.6
     v = -1.2542 + 0.7877 * v * 3.6
@@ -83,9 +88,24 @@ phase = np.random.random(N)
 u_phase = np.random.random(N)
 v_phase = np.random.random(N)
 
-genesis_sampler = SamplingOrigin(
-    kdeOrigin="/g/data/fj6/TCRM/TCHA18/process/originPDF.nc"
-)
+dataFile = os.path.join("/scratch/w85/kr4383/IDCKMSTM0S.csv")
+usecols = [0, 1, 2, 7, 8, 16, 49, 53]
+colnames = ['NAME', 'DISTURBANCE_ID', 'TM', 'LAT', 'LON',
+            'CENTRAL_PRES', 'MAX_WIND_SPD', 'MAX_WIND_GUST']
+dtypes = [str, str, str, float, float, float, float, float]
+
+df = pd.read_csv(dataFile, skiprows=4, usecols=usecols, dtype=dict(zip(colnames, dtypes)), na_values=[' '])
+df['TM'] = pd.to_datetime(df.TM, format="%Y-%m-%d %H:%M", errors='coerce')
+df = df[~pd.isnull(df.TM)]
+df['season'] = pd.DatetimeIndex(df['TM']).year - (pd.DatetimeIndex(df['TM']).month < 6)
+df = df[df.season >= 1981]
+df.reset_index(inplace=True)
+genesis_points = df.groupby('DISTURBANCE_ID').aggregate({'TM': 'first', 'LAT': 'first', 'LON': 'first'})
+tm = pd.DatetimeIndex(genesis_points.TM)
+genesis_points['TM'] = (tm.dayofyear * 24.0 + tm.hour)
+genesis_points = np.row_stack([genesis_points[c] for c in genesis_points.columns])
+pde = stats.gaussian_kde(genesis_points)
+
 pressure = np.array(
     [300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 775, 800, 825, 850]
 )
@@ -105,82 +125,85 @@ print("Starting simulation.")
 
 year = 2001  # arbitrary choice of non leap year
 
-for month in range(1, 13):
+climatologies = np.array([smooth(get_climatology(month)) for month in range(1, 13)]).swapaxes(0, 1)
+# udlm_2, vdlm_2 = load_dlm(year + (month // 12), (month % 12) + 1)
 
-    logging.info(f"Simulating tracks for {month}")
-    print(f"Simulating tracks for {month}")
+t0 = time.time()
 
-    climatology = get_climatology(month)
-    climatology = smooth(climatology)
-    # udlm_2, vdlm_2 = load_dlm(year + (month // 12), (month % 12) + 1)
+# sufficient repeats that the sum should be equal to the mean * number of repeats
 
-    t0 = time.time()
+revisit = []
 
-    # sufficient repeats that the sum should be equal to the mean * number of repeats
+days_in_month = monthrange(year, month)[1]
+longitudes = []
 
-    revisit = []
+num_events = int(sum(month_rates.values()) * repeats)
 
-    days_in_month = monthrange(year, month)[1]
-    longitudes = []
+durations = (np.round(stats.lognorm.rvs(0.5491, 0., 153.27, size=num_events))).astype(int)
 
-    num_events = int(np.round(month_rates[month] * repeats))
-    durations = (np.round(stats.lognorm.rvs(0.5491, 0., 153.27, size=num_events))).astype(int)
+samples = pde.resample(num_events)
+mask = (samples[2, :] >= 170) | (samples[2, :] <= 80)
 
-    year_month = np.datetime64(f'{year}-{month:02d}')
-    days = np.random.randint(1, days_in_month + 1, size=num_events) * np.timedelta64(1, 'D')
-    hours = np.random.randint(0, 24, size=num_events) * np.timedelta64(1, 'h')
-    timestamps = year_month + hours + days
+while mask.any():
+    samples[:, mask] = pde.resample(mask.sum())
+    mask = (samples[2, :] >= 170) | (samples[2, :] <= 80)
 
-    origin = genesis_sampler.generateSamples(num_events)
-    mask = (origin[:, 0] >= 170) | (origin[:, 0] <= 80)
-    while mask.any():
-        origin[mask, :] = genesis_sampler.generateSamples(mask.sum())
-        mask = (origin[:, 0] >= 170) | (origin[:, 0] <= 80)
+coords = np.empty((2, durations.max(), num_events)) * np.nan
 
-    coords = np.empty((2, durations.max(), num_events)) * np.nan
+latitudes = coords[0, ...]
+latitudes[0, :] = samples[1, :]
 
-    latitudes = coords[0, ...]
-    latitudes[0, :] = origin[:, 1]
+longitudes = coords[1, ...]
+longitudes[0, :] = samples[2, :]
 
-    longitudes = coords[1, ...]
-    longitudes[0, :] = origin[:, 0]
+tm = samples[0, :].astype(int) % (365 * 24)
+timestamps = pd.to_datetime('01-01-2001') + tm * pd.Timedelta(hours=1)
 
-    for step in range(durations.max() - 1):
+for step in range(durations.max() - 1):
 
-        mask = (longitudes[step] <= 170) & (longitudes[step] >= 80)
-        mask &= (latitudes[step] >= -40) & (latitudes[step] <= 0)
-        # mask &= timestamps <= udlm.coords['time'].data[-1]
-        mask &= step <= durations
+    mask = (longitudes[step] <= 170) & (longitudes[step] >= 80)
+    mask &= (latitudes[step] >= -40) & (latitudes[step] <= 0)
+    # mask &= timestamps <= udlm.coords['time'].data[-1]
+    mask &= step <= durations
 
-        long_idxs = np.round(4 * longitudes[step][mask]) - (4 * 80)
-        lat_idxs = np.round(4 * latitudes[step][mask])
+    long_idxs = np.round(4 * longitudes[step][mask]) - (4 * 80)
+    lat_idxs = np.round(4 * latitudes[step][mask])
 
-        idxs = (lat_idxs * climatology.shape[-1] + long_idxs).astype(int)
+    idxs = (lat_idxs * climatologies.shape[-1] + long_idxs).astype(int)
 
-        time_pd = pd.DatetimeIndex(timestamps)
-        t = time_pd.dayofyear * 24 + time_pd.hour
+    time_pd = pd.DatetimeIndex(timestamps)
+    t = time_pd.dayofyear * 24 + time_pd.hour
 
-        u, v = tc_velocity(climatology, t[mask], N, T, phase, idxs)
+    c = (time_pd.day < 15)  # if less than 30 days use current and previous month
+    month = time_pd.month
+    idxs1 = idxs + (month - 1) * climatologies[0, 0].size
+    idxs2 = idxs + c * ((month - 2) % 12) * climatologies[0, 0].size
+    idxs2 += (~c) * (month % 12) * climatologies[0, 0].size
 
-        dist = np.sqrt(u ** 2 + v ** 2)  # km travelled in one hour
+    weights2 = np.abs(time_pd.day - 15)
+    weights1 = 30 - weights1
 
-        mask1 = (u >= 0) & (v >= 0)
-        mask2 = (u >= 0) & (v < 0)
-        mask3 = (u < 0) & (v < 0)
+    u, v = tc_velocity(climatologies, t[mask], N, T, phase, idxs1, idxs2, weights1, weights2)
 
-        bearing = 360 + np.arctan(u / v) * 180 / np.pi
-        bearing[mask1] = (np.arctan(u / v) * 180 / np.pi)[mask1]
-        bearing[mask2] = (180 + np.arctan(u / v) * 180 / np.pi)[mask2]
-        bearing[mask3] = (180 + np.arctan(u / v) * 180 / np.pi)[mask3]
+    dist = np.sqrt(u ** 2 + v ** 2)  # km travelled in one hour
 
-        dest = destination(latitudes[step, mask], longitudes[step, mask], dist, bearing)
-        latitudes[step + 1, mask] = dest[0]
-        longitudes[step + 1, mask] = dest[1]
+    mask1 = (u >= 0) & (v >= 0)
+    mask2 = (u >= 0) & (v < 0)
+    mask3 = (u < 0) & (v < 0)
 
-        timestamps += np.timedelta64(1, 'h')
+    bearing = 360 + np.arctan(u / v) * 180 / np.pi
+    bearing[mask1] = (np.arctan(u / v) * 180 / np.pi)[mask1]
+    bearing[mask2] = (180 + np.arctan(u / v) * 180 / np.pi)[mask2]
+    bearing[mask3] = (180 + np.arctan(u / v) * 180 / np.pi)[mask3]
 
-    t1 = time.time()
+    dest = destination(latitudes[step, mask], longitudes[step, mask], dist, bearing)
+    latitudes[step + 1, mask] = dest[0]
+    longitudes[step + 1, mask] = dest[1]
 
-    print(f"Finished simulating tracks for {month}/{year}. Time taken: {t1 - t0}s")
-    logging.info(f"Finished simulating tracks for {month}/{year}. Time taken: {t1 - t0}s")
-    np.save(f"/scratch/w85/kr4383/climatology_tracks/climatology_tracks_{month}_{year}.npy", coords)
+    timestamps += np.timedelta64(1, 'h')
+
+t1 = time.time()
+
+print(f"Finished simulating tracks for {month}/{year}. Time taken: {t1 - t0}s")
+logging.info(f"Finished simulating tracks for {month}/{year}. Time taken: {t1 - t0}s")
+np.save(f"/scratch/w85/kr4383/climatology_tracks/climatology_tracks_{month}_{year}.npy", coords)
