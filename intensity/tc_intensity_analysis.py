fixed scaling bug

intensity/tc_intensity_analysis.py
@@ -25,7 +25,9 @@ from Utilities.loadData import getPoci
 print("Done imports")
 
 OCEAN_MIXING = False
-USE_SHEAR = False
+USE_SHEAR = True
+SHEAR_CONST = 300.0
+NN = 5
 
 
 class Hurricane:
@@ -34,7 +36,11 @@ class Hurricane:
     This contains the state and parameters of the TC intensity model for the simulation of one TC.
     """
 
-    def __init__(self, dt=1, efrac=0.7, cecd=1.0, ocean_mixing=False, use_shear=False, tinit=2.0):
+    def __init__(
+            self, dt=1, efrac=0.7, cecd=1.0,
+            ocean_mixing=False, use_shear=False, tinit=2.0,
+            shear_const=300.0,
+    ):
         """
         Params:
 
@@ -54,6 +60,8 @@ class Hurricane:
         self.uhmix1, self.uhmix2, self.sst1, self.sst2, self.hmix = [
             np.zeros(200, dtype=np.float32) for _ in range(5)
         ]
+        self.ps2 = np.zeros(200, dtype=np.float32)
+        self.ps3 = np.zeros(200, dtype=np.float32)
 
         self.ro = 1200 # km
         self.v = np.zeros(200, dtype=np.float32)
@@ -68,8 +76,9 @@ class Hurricane:
         self.use_shear = use_shear
         self.ocean_mixing = ocean_mixing
         self.tinit = tinit
+        self.shear_const = shear_const
 
-    def simulate(self, g, verbose=False):
+    def simulate(self, g, verbose=False, freeze=False):
         """
         Runs the TC simulation for a df of times, locations, and environmental conditions
 
@@ -82,7 +91,7 @@ class Hurricane:
         row = g.iloc[0]
 
         sst, sp, hm, lat, ahm, ut, hm, h_a, *_ = self.extract_environment(row)
-        vm = row["Vmax (kn)"] * 0.514444  # convert knots to m/s
+        vm = 20.0 # row["Vmax (kn)"] * 0.514444  # convert knots to m/s
 
         dP = find_dP(vm, lat)
         rm = rmax(dP, lat, eps=0)
@@ -98,8 +107,11 @@ class Hurricane:
         any_bailed = False
         diagnostic_info = []
         for j, i in enumerate(g.index[:-1]):
-            row = g.loc[i]
-            sst, sp, hm, lat, ahm, ut, hm, h_a, dsst, gm, max_depth, shear = self.extract_environment(row)
+            if freeze:
+                row = g.iloc[0]
+            else:
+                row = g.iloc[j]
+            sst0, sp0, hm0, lat0, ahm0, ut0, hm0, h_a0, dsst0, gm0, max_depth0, shear0 = self.extract_environment(row)
 
             if np.isnan(hm) or globe.is_land(row.LAT, row.LON):
                 break
@@ -112,15 +124,44 @@ class Hurricane:
 
             if j == 0:
                 bailed, out = self.pytc_intensity(
-                    vm, rm, r0, sst, h_a, abs(lat), ahm, sp, self.tinit, ut, hm=hm, match='y', vobs=vm, gamma=100_000
+                    vm, rm, r0, sst0, h_a0, abs(lat0), ahm0, sp0, self.tinit, ut0, hm=hm0, match='y', vobs=vm,
+                    gamma=100_000, first=True,
                 )
                 pmin, vm, rm = out
+                mask = self.x1 != 0
+                diagnostic_info.append([vm, rm, np.nanmax((self.xm1 - self.xm0)[mask] / (self.x1 - self.xm0)[mask])])
 
-            bailed, out = self.pytc_intensity(vm, rm, r0, sst, h_a, abs(lat), ahm, sp, tend, ut, hm=hm, gm=gm, dsst=dsst, shear=shear)
-            any_bailed |= bailed
+            sst1, sp1, hm1, lat1, ahm1, ut1, hm, h_a1, dsst1, gm1, max_depth1, shear1 = self.extract_environment(
+                g.loc[g.index[j + 1]]
+            )
 
-            pmin, vm, rm = out[0], out[1], out[2]
-            diagnostic_info.append([vm, rm, sst, float(h_a), ahm, tend])
+            n = NN
+            for ii in range(n):
+                ii = 0
+                sst = sst0 + (sst1 - sst0) * (ii / n)
+                lat = lat0 + (lat1 - lat0) * (ii / n)
+                h_a = h_a0 + (h_a1 - h_a0) * (ii / n)
+                ahm = ahm0 + (ahm1 - ahm0) * (ii / n)
+                sp = sp0 + (sp1 - sp0) * (ii / n)
+                ut = ut0 + (ut1 - ut0) * (ii / n)
+                hm = hm0 + (hm1 - hm0) * (ii / n)
+                gm = gm0 + (gm1 - gm0) * (ii / n)
+                dsst = dsst0 + (dsst1 - dsst0) * (ii / n)
+                shear = shear0 + (shear1 - shear0) * (ii / n)
+                
+                bailed, out = self.pytc_intensity(
+                    vm, rm, r0, sst, h_a, abs(lat), ahm, sp, tend / n, 
+                    ut, hm=hm, gm=gm, dsst=dsst, shear=shear,
+                )
+                print((
+                    vm, rm, r0, sst, h_a, abs(lat), ahm, sp, tend / n,
+                    ut, hm, gm, dsst, shear,
+                ))
+                any_bailed |= bailed
+                pmin, vm, rm = out[0], out[1], out[2]
+
+            mask = self.x1 != 0
+            diagnostic_info.append([vm, rm, np.nanmax((self.xm1 - self.xm0)[mask] / (self.x1 - self.xm0)[mask])])
 
             out_rows.append(
                 [
@@ -136,11 +177,11 @@ class Hurricane:
 
         if verbose:
             diagnostic_info = pd.DataFrame(diagnostic_info)
-            diagnostic_info.columns = ["vm", "rm", "sst", "ssh", "troph", "tend"]
+            diagnostic_info.columns = ["vm", "rm", "precip"]
             print("Final vm:", vm)
             print(diagnostic_info)
-            print("\n" * 3)
-            print(out_df)
+            # print("\n" * 3)
+            # print(out_df)
 
         return any_bailed, out_df
 
@@ -150,8 +191,8 @@ class Hurricane:
         A helper function to extract the environmental conditions from a row of the input DataFrame of the simulation
         """
         sst, sp, d2, t2, hm, lat = row.sst, row.sp, row.d2, row.t2, row.hm, row.LAT
-        #dsst, gm, max_depth, shear = row.dsst, row.gm, row.max_depth, row.shear
-        dsst = gm = max_depth = shear = 0
+        dsst, gm, max_depth, shear = row.dsst, row.gm, row.max_depth, row.shear
+        # dsst = gm = max_depth = shear = 0
         ahm, ut, hm = row.rh, row.vfm, row.hm
 
         dsst = max(dsst, 0)
@@ -169,7 +210,7 @@ class Hurricane:
     def pytc_intensity(
             self, vm, rm, r0, ts, h_a, alat, ahm, pa, tend,
             ut, hm=30.0, dsst=0.6, gm=8.0, match='n', vobs=0, gamma=1000,
-            max_depth=np.inf, shear=0
+            max_depth=np.inf, shear=0, first=False,
     ):
         """
         This function runs the simulation for fixed environmental conditions.
@@ -255,12 +296,16 @@ class Hurricane:
 
         v1 = 0.5 * f * (r * r - self.rbs2[:self.nr]) / np.sqrt(self.rbs2[:self.nr])
 
+        tm = 0.85 * ts + 0.15 * to
+        tma = tm + 273.15
+        xm0 = 2.5e6 * (ts - to) * qsm * (ahm - 1.) / tma
+        self.xm0 = xm0
         for arr in (self.x1, self.xs1, self.xm1, self.x2, self.xs2, self.xm2):
             arr /= chi
-
+        #
         for arr in (self.rbs1, self.rts1, self.rbs2, self.rts2):
             arr /= (np.sqrt(chi) / f) ** 2
-
+        #
         for arr in (self.mu1, self.mu2):
             arr /= cd * np.sqrt(chi)
 
@@ -273,7 +318,7 @@ class Hurricane:
         ahm /= 0.01
         cd /= 0.001
         gm /= 0.01
-
+        vobs /= np.sqrt(chi)
         out = np.zeros(4, dtype=np.float32)
         tc_intensity(
             nrd, tend, vm,
@@ -282,24 +327,21 @@ class Hurricane:
             cecd, pnu, taur, radmax, tauc, efrac, dpb, hm, dsst, gm, out,
             self.rbs1, self.rts1, self.x1, self.xs1, self.xm1, self.mu1,
             self.rbs2, self.rts2, self.x2, self.xs2, self.xm2, self.mu2,
-            self.uhmix1, self.uhmix2, self.sst1, self.sst2, self.hmix, self.init,
-            match, vobs, gamma, self.diagnostic
+            self.uhmix1, self.uhmix2, self.sst1, self.sst2, self.ps2, self.ps3,
+            self.hmix, self.init, match, vobs, gamma, self.shear_const, self.diagnostic
         )
         bailed = (out[3] == 1) or np.isnan(self.rbs1).any()
 
         out = out[:3]
         self.init = 'n'
-        h_a *= 0.01
-        ahm *= 0.01
-        cd *= 0.001
-        gm *= 0.01
 
+        cd *= 0.001
         for arr in (self.x1, self.xs1, self.xm1, self.x2, self.xs2, self.xm2):
             arr *= chi
-
+        #
         for arr in (self.rbs1, self.rts1, self.rbs2, self.rts2):
             arr *= (np.sqrt(chi) / f) ** 2
-
+        #
         for arr in (self.mu1, self.mu2):
             arr *= cd * np.sqrt(chi)
 
@@ -309,7 +351,6 @@ class Hurricane:
 
         v = 0.5 * f * (r * r - self.rbs2[:self.nr]) / np.sqrt(self.rbs2[:self.nr])
         # print(v[1:self.nr].max(), out[1], "\n")
-
         return bailed, out
 
 
@@ -371,15 +412,12 @@ if __name__ == "__main__":
         dssts.append(dsst)
         max_depths.append(max_depth)
 
-    mslpFile = os.path.join(TCRM_PATH, "MSLP", "slp.day.ltm.nc")
-    mslp = SamplePressure(mslpFile)
-
     for i, var in enumerate(["sst", "sp", "d2", "t2"]): df[var] = era5[:, i]
     df["hm"] = mld[:, 0]
     df['rh'] = rh[:, 23]  # np.trapz(rh[:, pres_lvls >= 200], pres_lvls[pres_lvls >= 200]) / (800)
-    df['gm'] = np.array(gm)
-    df['dsst'] = np.array(gm)
-    df['max_depth'] = np.array(gm)
+    df['gm'] = -np.array(gms)
+    df['dsst'] = np.array(dssts)
+    df['max_depth'] = np.array(max_depths)
     df['shear'] = shear
 
     df = df[~pd.isnull(df["Vmax (kn)"])]
@@ -395,20 +433,21 @@ if __name__ == "__main__":
         verbose = False
         if len(g) == 0:
             continue
-        # if name == 'AU200405_10U':
-        #     print(name)
-        # else:
-        #     continue
-        hurricane = Hurricane(dt=0.25)
+        if name == 'AU199394_01U':
+            print(name)
+            verbose=True
+        else:
+            continue
+        hurricane = Hurricane(dt=1.0, ocean_mixing=OCEAN_MIXING, use_shear=USE_SHEAR, shear_const=SHEAR_CONST)
         bailed, out = hurricane.simulate(g, verbose=verbose)
         print("max windspeed:", out.vmax.max())
         out_dfs.append(out)
 
     out_df = pd.concat(out_dfs)
-    out_fn = f"predicted_intensity{'_shear' if USE_SHEAR else ''}{'_ocean_mixing' if OCEAN_MIXING else ''}.csv"
+    out_fn = f"test_smooth_{NN}_predicted_intensity{f'_shear_{SHEAR_CONST}' if USE_SHEAR else ''}{'_ocean_mixing' if OCEAN_MIXING else ''}.csv"
     #out_fn = f"extreme_lmi_0.5.csv"
 
-    out_df.to_csv(os.path.join(DATA_DIR, out_fn))
+    # out_df.to_csv(os.path.join(DATA_DIR, out_fn))
 
     print(land_count)
     print("Time: ", (time.time() - t0), "s")