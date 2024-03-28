c79595931f92d66b7150e07a2fbaba95f38cd1f9
wcarthur <wcarthur@gmail.com>
2024-03-28 15:51:23 +1100

    Analyse longitudinal dependence

--- synoptic-flow/fitBAMparameters.py
+++ synoptic-flow/fitBAMparameters.py
@@ -11,6 +11,7 @@ import glob
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
 import lmfit
 import seaborn as sns
 from datetime import datetime
@@ -21,6 +22,7 @@ BASEDIR = "/scratch/w85/cxa547/envflow"
 filelist = sorted(glob.glob(os.path.join(BASEDIR, "tcenvflow_serial.*.csv")))
 df = pd.concat((pd.read_csv(f) for f in filelist), ignore_index=True)
 
+df.loc[df['LON']<0, "LON"] = df['LON'] + 360
 df.dropna(subset=['u850', 'u250', 'v850', 'v250'], inplace=True)
 
 # Define the different models that will be used:
@@ -100,6 +102,8 @@ def plot_scatter(df, filename):
 
 df['band'] = pd.cut(df['MAX_WIND_SPD'], np.arange(10, 71, 5),
                     labels=np.arange(10, 70, 5))
+df['lonband'] = pd.cut(df['LON'], np.arange(30, 250.1, 20),
+                       labels=np.arange(40, 250, 20))
 
 def fit_model(df):
     resdf = pd.DataFrame(columns=['i', 'au', 'av', 'aust', 'avst',
@@ -131,6 +135,35 @@ def fit_model(df):
             vfresult.rsquared]
     return resdf
 
+def fit_model_longitude(df):
+    resdf = pd.DataFrame(columns=['i', 'au', 'av', 'aust', 'avst',
+                                  'ursq', 'ufrsq', 'vrsq', 'vfrsq'])
+
+    for i, x in enumerate(np.arange(40, 250.1, 20)):
+        ddf = df[df['lonband']==x]
+        model = lmfit.Model(twolevel_model, independent_vars=['xl', 'xu'])
+        u = ddf['u'] # Observed u component
+        ul = ddf['u850'] # Low-level u component of environmental flow
+        uu = ddf['u250'] # Upper-level u component of environmental flow
+        v = ddf['v']
+        vl = ddf['v850']
+        vu = ddf['v250']
+        uresult = rmod.fit(u, p, xl=ul, xu=uu)
+        vresult = rmod.fit(v, p, xl=vl, xu=vu)
+        ufresult = fmod.fit(u, p, xl=ul, xu=uu)
+        vfresult = fmod.fit(v, p, xl=vl, xu=vu)
+
+        resdf.loc[len(resdf.index)] = [
+            x,
+            uresult.params['alpha'].value,
+            vresult.params['alpha'].value,
+            uresult.params['alpha'].stderr,
+            vresult.params['alpha'].stderr,
+            uresult.rsquared,
+            ufresult.rsquared,
+            vresult.rsquared,
+            vfresult.rsquared]
+    return resdf
 
 def fit_alpha_intensity(df):
     # Fit a simple linear regression to the resulting fits of $\alpha$ at
@@ -233,7 +266,7 @@ def plotModel(df, fitdf, filename):
             transform=fig.transFigure, ha='right', fontsize='xx-small')
     plt.savefig(os.path.join(BASEDIR, filename),
                 bbox_inches='tight')
-    
+
     fig, ax = plt.subplots(1, 1, figsize=(6, 6))
     for i, x in enumerate(np.arange(10, 70, 5)):
         ualpha = fitdf.loc[i, 'au']
@@ -248,7 +281,7 @@ def plotModel(df, fitdf, filename):
 
         upred = ualpha*ul + (1 - ualpha)*uu
         vpred = valpha*vl + (1 - valpha)*vu
-        
+
         magu = np.sqrt(u**2 + v**2)
         magupred = np.sqrt(upred**2 + vpred**2)
         ax.plot(magupred, magu, 'o', label=f"{x} m/s")
@@ -266,10 +299,93 @@ def plotModel(df, fitdf, filename):
     plt.savefig(os.path.join(BASEDIR, "mag_"+filename),
                 bbox_inches='tight')
 
+def plotModelLongitude(df, fitdf, filename):
+    fig, ax = plt.subplots(1, 2, figsize=(12, 6), sharey=True)
+    for i, x in enumerate(np.arange(40, 240.1, 20)):
+        ualpha = fitdf.loc[i, 'au']
+        valpha = fitdf.loc[i, 'av']
+        ddf = df[df['lonband']==x]
+        u = ddf['u'] # Observed u component
+        ul = ddf['u850'] # Low-level u component of environmental flow
+        uu = ddf['u250'] # Upper-level u component of environmental flow
+        v = ddf['v']
+        vl = ddf['v850']
+        vu = ddf['v250']
+
+        upred = ualpha*ul + (1 - ualpha)*uu
+        vpred = valpha*vl + (1 - valpha)*vu
+        ax[0].plot(upred, u, 'o', label=f"{x}E")
+        ax[1].plot(vpred, v, 'o')
+
+    ax[0].set_ylim((-15, 15))
+    ax[0].set_xlim((-15, 15))
+    ax[0].set_xlabel(r"Predicted $u_t$ [m/s]")
+    ax[0].set_ylabel(r"Observed $u_t$ [m/s]")
+    ax[0].grid()
+    #ax[0].legend()
+    ax[0].text(0.05, 0.95, r"$u_t$", transform=ax[0].transAxes,
+            fontweight='bold', va='top')
+    ax[1].set_ylim((-15, 15))
+    ax[1].set_xlim((-15, 15))
+    ax[1].grid()
+    ax[1].yaxis.set_label_position("right")
+    ax[1].yaxis.tick_right()
+    ax[1].set_ylabel(r"Observed $v_t$ [m/s]")
+    ax[1].set_xlabel(r"Predicted $v_t$ [m/s]")
+    ax[1].text(0.05, 0.95, r"$v_t$", transform=ax[1].transAxes,
+            fontweight='bold', va='top')
+    plt.figlegend(bbox_to_anchor=(.5, 0), loc="lower center",
+                bbox_transform=fig.transFigure, ncol=4,
+                title=r"Longitude [$^{\circ}$E]")
+    fig.subplots_adjust(bottom=0.25)
+    plt.text(0.95, 0.025, f"Created: {datetime.now():%Y-%m-%d %H:%M %z}",
+            transform=fig.transFigure, ha='right', fontsize='xx-small')
+    plt.savefig(os.path.join(BASEDIR, filename),
+                bbox_inches='tight')
+
+def plotResultsLongitude(df, filename):
+    x = np.arange(40, 240.1, 20)
+    #lresult = fit_alpha_intensity(df)
+    fig, axes = plt.subplots(2, 1, figsize=(8, 8), sharex=True)
+    axes[0].errorbar(df['i'], df['au'], yerr=df['aust'],
+                    color='b', capsize=5, label='Zonal')
+    axes[0].errorbar(df['i'], df['av'], yerr=df['avst'],
+                    color='r', capsize=5, label='Meridional')
+    axes[0].plot(df['i'], (df['au']+df['av'])/2., color='k', label="Mean")
+    #axes[0].plot(x[:-3], lresult.best_fit, color='k',
+    #             linestyle='--', label="Linear fit")
+    axes[0].grid(True)
+    axes[0].set_ylim((0.0, 1.0))
+    axes[0].set_xlim((40, 240))
+    axes[0].xaxis.set_major_locator(MultipleLocator(20))
+    axes[0].xaxis.set_minor_locator(MultipleLocator(10))
+
+    axes[0].set_ylabel(r"$\alpha(\lambda)$")
+    axes[0].legend(ncols=2)
+
+
+    axes[1].plot(x, df['ursq'], color='b')
+    axes[1].plot(x, df['vrsq'], color='r', label=r"$\alpha (v)$")
+    axes[1].plot(x, df['ufrsq'], color='b',
+                 linestyle='--', label=r"Constant $\alpha$")
+    axes[1].plot(x, df['vfrsq'], color='r', linestyle='--')
+    axes[1].grid(True)
+    axes[1].set_ylim((-1, 1))
+    axes[1].set_ylabel(r"$r^{2}$")
+    axes[1].set_xlabel(r"Longitude [$^{\circ}$E]")
+    axes[1].legend(ncols=2)
+    plt.text(0.95, 0.05, f"Created: {datetime.now():%Y-%m-%d %H:%M %z}",
+            transform=fig.transFigure, ha='right')
+    plt.savefig(os.path.join(BASEDIR, filename), bbox_inches='tight')
+
 fitdf = fit_model(df)
+fitdf_lon = fit_model_longitude(df)
 plot_scatter(df, filename="tcenvflow_scatter.png")
 plotResults(fitdf, filename="tcenvflow_fit.png")
+plotResultsLongitude(fitdf_lon, filename="tcenvflow_fit.longitude.png")
 plotModel(df, fitdf, filename="tcenvflow_fullfit.png")
+plotModelLongitude(df, fitdf_lon, filename="tcenvflow_fullfit.longitude.png")
+
 df.drop("Unnamed: 0", axis=1).to_csv(os.path.join(BASEDIR, "tcenvflow.csv"), index=False)
 fitdf.to_csv(os.path.join(BASEDIR, "tcenvflow.fitstats.csv"), index=False)
 for basin in df['BASIN'].unique():
