Correct for different averaging periods in envflow.py, add plotBetaDrift.py

synoptic-flow/envflow.py
@@ -5,6 +5,11 @@ Using ERA5 reanalysis of 850 and 250 hPa sub-daily (6 hours) winds,
 calculate the environmental flow in the vicinity of each TC
 observation.
 
+Requires the ERA5 data to be extracted from the ERA5 replication dataset
+on the NCI. This has been done using `extract_era5.py`. We rotate the 
+longitude coordinate to the range [0, 360) to avoid having the grid boundary
+at the dateline (which cuts across the South Pacific basin). 
+
 The steering wind is calculated using "vortex surgery" to remove the
 TC vortex from the reanalysis data to determine the background flow.
 The details of this process are described in Galarneau and Davis (2013).
@@ -35,6 +40,14 @@ https://scitools.org.uk/cartopy/docs/latest/gallery/scalar_data/wrapping_global.
 windspharm - for vector wind calculations using spherical harmonics.
 https://ajdawson.github.io/windspharm/latest/
 
+References:
+Galarneau, T. J., and C. A. Davis, 2013: Diagnosing Forecast Errors in Tropical
+Cyclone Motion. Monthly Weather Review, 141, 405-430, 
+https://doi.org/10.1175/MWR-D-12-00071.1
+
+Hersbach, H., and Coauthors, 2020: The ERA5 global reanalysis. Quarterly Journal
+of the Royal Meteorological Society, 146, 1999-2049, 
+https://doi.org/10.1002/qj.3803
 
 Author: Craig Arthur
 
@@ -67,7 +80,7 @@ import pyproj
 # BoM best track and IBTrACS are stored in the DATA_DIR folder
 
 geodesic = pyproj.Geod(ellps="WGS84")
-WIDTH = 6.25
+WIDTH = 4.0
 DATA_DIR = "/g/data/w85/data/tc"
 DLM_DIR = "/scratch/w85/cxa547/tcr/data/era5"
 OUT_DIR = "/scratch/w85/cxa547/envflow/cyclic"
@@ -154,6 +167,9 @@ def load_ibtracs_df(season, basins=['SI', 'SP']):
     idxs = df.groupby(["DISTURBANCE_ID"]).agg(
         {"new_index": "max"}).values.flatten()
     df.drop("new_index", axis=1, inplace=True)
+    # Replace any missing values with the WMO source data, converted to a 1-minute sustained wind speed
+    # Only considering SI and SP basins, where the WMO sources *should* be 10-minute means!
+    df.fillna({"MAX_WIND_SPD": df["WMO_WIND"] * 1.029}, inplace=True)
     # Convert max wind speed to m/s for consistency
     df["MAX_WIND_SPD"] = (df["MAX_WIND_SPD"] * 0.5144)
 
@@ -261,7 +277,7 @@ def solvePoisson(scalar):
     return uu, vv
 
 
-def extract_steering(uda, vda, cLon, cLat, dx, dy, width=4):
+def extract_steering(uda, vda, cLon, cLat, dx, dy, width=4.0):
     """
     Calculate the steering flow for a region centred at `cLon`, `cLat`.
 
@@ -331,7 +347,7 @@ def load_steering(uda, vda, df, width=4.0):
                minimum the time, latitude and longitude of the TC positions,
                named as 'TM', 'LAT', 'LON' respectively.
     :param width: Size of the box over which to calculate mean steering flow.
-                  Default is 4 degrees. Loosely based on the 400 km used in
+                  Default is 4 degrees. Based on the 400 km used in
                   Lin et al. (2023) and Galarneau and Davis (2013).
     """
     griddx, griddy = lat_lon_grid_deltas(
@@ -384,7 +400,7 @@ def process(season):
     """
     df = load_ibtracs_df(season, BASINS)
     print("extract steering current from environmental flow:")
-    results = load_steering(uda, vda, df)
+    results = load_steering(uda, vda, df, width=WIDTH)
 
     cols = ["index", "u850", "u250", "v850", "v250"]
     vdf = pd.DataFrame(data=np.array(

synoptic-flow/fitBAMparameters.py
@@ -1,7 +1,14 @@
 """
 Fit beta-advection model parameters to TC motion
 
+This is used in conjunction with extract_era5.py and envflow.py
+to calculate the BAM parameters described in Lin et al. 2023.
 
+References:
+Lin, J., R. Rousseau-Rizzi, C.-Y. Lee, and A. Sobel, 2023: An Open-Source,
+Physics-Based, Tropical Cyclone Downscaling Model With Intensity-Dependent
+Steering. Journal of Advances in Modeling Earth Systems, 15, e2023MS003686,
+https://doi.org/10.1029/2023MS003686.
 
 Author: Craig Arthur
 2024-03-20
@@ -38,7 +45,7 @@ def savefig(filename, *args, **kwargs):
 BASEDIR = "/scratch/w85/cxa547/envflow/cyclic"
 filelist = sorted(glob.glob(os.path.join(BASEDIR, "tcenvflow_serial.*.csv")))
 df = pd.concat((pd.read_csv(f) for f in filelist), ignore_index=True)
-
+df['MAX_WIND_SPD'].fillna(df['WMO_WIND'], inplace=True)
 df.loc[df['LON'] < 0, "LON"] = df['LON'] + 360
 df.dropna(subset=['u850', 'u250', 'v850', 'v250'], inplace=True)
 
@@ -307,6 +314,8 @@ def plotResults(df, filename):
 
 def plotModel(df, fitdf, filename):
     fig, ax = plt.subplots(1, 2, figsize=(12, 6), sharey=True)
+    df['upred'] = np.nan
+    df['vpred'] = np.nan
     for i, x in enumerate(np.arange(10, 70, 5)):
         ualpha = fitdf.loc[i, 'au']
         valpha = fitdf.loc[i, 'av']
@@ -320,6 +329,8 @@ def plotModel(df, fitdf, filename):
 
         upred = ualpha*ul + (1 - ualpha)*uu
         vpred = valpha*vl + (1 - valpha)*vu
+        df.loc[df['band'] == x, 'upred'] = upred
+        df.loc[df['band'] == x, 'vpred'] = vpred
         ax[0].plot(upred, u, 'o', label=f"{x} m/s")
         ax[1].plot(vpred, v, 'o')
 
@@ -375,6 +386,11 @@ def plotModel(df, fitdf, filename):
     fig.subplots_adjust(bottom=0.25)
     savefig(os.path.join(BASEDIR, "mag_"+filename),
             bbox_inches='tight')
+    
+    # Save predicted translation vector to file:
+    df.drop("Unnamed: 0", axis=1).to_csv(
+        os.path.join(BASEDIR, "tcenvflow.pred.csv"),
+        index=False)
 
 
 def plotModelLongitude(df: pd.DataFrame, fitdf: pd.DataFrame, filename: str):
@@ -484,15 +500,15 @@ fitdf.to_csv(
     index=False
     )
 
-for basin in df['BASIN'].unique():
-    print(basin)
-    basinfit = fit_model(df[df['BASIN'] == basin])
-    basinfit.to_csv(
-        os.path.join(BASEDIR, f"tcenvflow.fitstats.{basin}.csv"),
-        index=False)
-    plot_scatter(df[df['BASIN'] == basin],
-                 filename=f"tcenvflow_scatter.{basin}.png")
+#for basin in df['BASIN'].unique():
+#    print(basin)
+#    basinfit = fit_model(df[df['BASIN'] == basin])
+#    basinfit.to_csv(
+#        os.path.join(BASEDIR, f"tcenvflow.fitstats.{basin}.csv"),
+#        index=False)
+#    plot_scatter(df[df['BASIN'] == basin],
+#                 filename=f"tcenvflow_scatter.{basin}.png")
 
-    plotResults(basinfit, filename=f"tcenvflow_fit.{basin}.png")
-    plotModel(df[df['BASIN'] == basin], basinfit,
-              filename=f"tcenvflow_fullfit.{basin}.png")
+#    plotResults(basinfit, filename=f"tcenvflow_fit.{basin}.png")
+#    plotModel(df[df['BASIN'] == basin], basinfit,
+#              filename=f"tcenvflow_fullfit.{basin}.png")