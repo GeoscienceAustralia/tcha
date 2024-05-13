3210c96e1bb044c9a6f6f87eb0cfd38d1680f547
Craig Arthur <craig.arthur@ga.gov.au>
2024-05-13 13:38:47 +1000

    Change base model formulation

--- synoptic-flow/fitBetaEnvironmentalParameters.R
+++ synoptic-flow/fitBetaEnvironmentalParameters.R
@@ -6,23 +6,23 @@ df$dvdx <- df$dvdx * 10e5
 df$dzdy <- df$dzdy * 10e11
 df$dudy2 <- df$dudy**2
 df$dvdx2 <- df$dvdx**2
-lmu <- lm(ub ~ dudy + dvdx + dudy2 + dvdx2 + dzdy + su + sv + lat, data=df)
-lmv <- lm(vb ~ dudy + dvdx + dudy2 + dvdx2 + dzdy + su + sv + lat, data=df)
+lmu <- lm(ub ~ dudy + dvdx + dudy2 + dzdy + su + sv, data=df)
+lmv <- lm(vb ~ dudy + dvdx + dudy2 + dzdy + su + sv, data=df)
 
 bestlmu <- stepAIC(lmu, direction = "both")
 bestlmv <- stepAIC(lmv, direction = "both")
 
-predub <- predict(bestlmu)
+predub <- predict(bestlmu, df)
 residub <- residuals(bestlmu)
 
 # Extract predicted values and residuals for Model 2
-predvb <- predict(bestlmv)
+predvb <- predict(bestlmv, df)
 residvb <- residuals(bestlmv)
 
 par(mfrow = c(1, 2))
 
 plot(predub, df$ub,
-     xlab = expression(paste("Predicted values (u_beta)")),
+     xlab = "Predicted values (u_beta)",
      ylab = "Observed (u_beta)",
      main = "Observed vs. Predicted (u_beta)")
 abline(a = 0, b = 1, col = "red")  # Add horizontal line at y = 0
