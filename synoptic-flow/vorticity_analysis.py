3678a41debf4e622562cc0f9d924cc8b69db6f68
wcarthur <wcarthur@gmail.com>
2024-05-22 17:19:14 +1000

    Add extraction of vorticity, zonal & meridional gradients at TC locations

--- synoptic-flow/vorticity_analysis.py
+++ synoptic-flow/vorticity_analysis.py
@@ -37,6 +37,9 @@ from windspharm.xarray import VectorWind
 
 DATA_DIR = "/g/data/w85/data/tc"
 geodesic = pyproj.Geod(ellps="WGS84")
+
+# Set spectral truncation to T30 (approx. 4 degrees)
+TRUNCATION = 30
 proj = ccrs.PlateCarree(central_longitude=180)
 trans = ccrs.PlateCarree()
 
@@ -198,28 +201,28 @@ ds['lon'] = ds['lona']
 ua850 = ds['ua850_Mean']
 va850 = ds['va850_Mean']
 w850 = VectorWind(ua850, va850, legfunc="computed")
-vrt850, div850 = w850.vrtdiv(truncation=17)
+vrt850, div850 = w850.vrtdiv(truncation=TRUNCATION)
 # Calculate gradient of vorticity:
 vrt850x, vrt850y = w850.gradient(vrt850)
 
 ua250 = ds['ua250_Mean']
 va250 = ds['va250_Mean']
 w250 = VectorWind(ua250, va250, legfunc="computed")
-vrt250, div250 = w250.vrtdiv(truncation=17)
+vrt250, div250 = w250.vrtdiv(truncation=TRUNCATION)
 vrt250x, vrt250y = w250.gradient(vrt250)
 
 # Calculate vorticity/divergence for deep layer mean. 
 # Use simplified model of 0.8u_850 + 0.2u_250.
-# Also calculate meridional and zonal shear of the DLM vorticity and 
+# Also calculate meridional and zonal gradient of the DLM vorticity and 
 # components.
 # See Zhao et al. (2009) https://doi.org/10.1029/2009GL040126
 uDLM = 0.8 * ua850 + 0.2 * ua250
 vDLM = 0.8 * va850 + 0.2 * va250
 wDLM = VectorWind(uDLM, vDLM, legfunc="computed")
-vrtDLM, divDLM = wDLM.vrtdiv(truncation=17)
+vrtDLM, divDLM = wDLM.vrtdiv(truncation=TRUNCATION)
 vrtDLMx, vrtDLMy = wDLM.gradient(vrtDLM)
-duDLMdx, duDLMdy = wDLM.gradient(uDLM)
-dvDLMdx, dvDLMdy = wDLM.gradient(vDLM)
+duDLMdx, duDLMdy = wDLM.gradient(uDLM, truncation=TRUNCATION)
+dvDLMdx, dvDLMdy = wDLM.gradient(vDLM, truncation=TRUNCATION)
 
 # Enable plotting across the dateline:
 vrt850c, clon, clat = cutil.add_cyclic(vrt850, vrt850.lon, vrt850.lat)
@@ -748,8 +751,8 @@ for i, l in enumerate(lons):
     lonslice = slice(l-5, l+5)
     y850sh[i, :] = vrt850ysh.sel(lon=lonslice, time=vrt850y.time.dt.season=="DJF").mean(dim=["lon", "time"])
     y850shstd[i, :] = vrt850ysh.sel(lon=lonslice, time=vrt850y.time.dt.season=="DJF").mean(dim="lon").std(dim="time")
-    y850nh[i, :] = vrt850ysh.sel(lon=lonslice, time=vrt850y.time.dt.season=="JJA").mean(dim=["lon", "time"])
-    y850nhstd[i, :] = vrt850ysh.sel(lon=lonslice, time=vrt850y.time.dt.season=="JJA").mean(dim="lon").std(dim="time")
+    y850nh[i, :] = vrt850ynh.sel(lon=lonslice, time=vrt850y.time.dt.season=="JJA").mean(dim=["lon", "time"])
+    y850nhstd[i, :] = vrt850ynh.sel(lon=lonslice, time=vrt850y.time.dt.season=="JJA").mean(dim="lon").std(dim="time")
     
 # Plot mean meridional gradient of vorticity by latitude, for each longitude:
 fig, ax = plt.subplots(2, 1, figsize=(12, 6), sharex=True)
@@ -874,17 +877,31 @@ savefig("vortgrad.850.longitude.profile.png")
 
 # Calculate climatological mean gradients of vorticity, zonal and meridional flow
 # for all storms in the Southern Hemisphere. This is to test the results of
-# Zhao et al. (2009), https://doi.org/10.1029/2009GL040126
-vrtDLMxsh_djf850 = vrtDLMx.sel(time=vrtDLMx.time.dt.season=="DJF")
+# Zhao et al. (2009), https://doi.org/10.1029/2009GL040126. 
+# We calculate climatological mean as the DJFM (southern hemisphere) or JJAS 
+# (northern hemisphere) mean of the deep-layer mean (0.8*u_850 + 0.2*u_250)
+# within 4 degrees of the centre of the cyclone. 
+vrtDLMxsh_djf850 = vrtDLMx.sel(time=vrtDLMx.time.dt.month.isin([12, 1, 2, 3]))
 vrtDLMxsh_seasonal = vrtDLMxsh_djf850.groupby(vrtDLMxsh_djf850.time.dt.year).mean("time").mean(axis=0)
-vrtDLMysh_djf850 = vrtDLMy.sel(time=vrtDLMy.time.dt.season=="DJF")
+vrtDLMysh_djf850 = vrtDLMy.sel(time=vrtDLMy.time.dt.month.isin([12, 1, 2, 3]))
 vrtDLMysh_seasonal = vrtDLMysh_djf850.groupby(vrtDLMysh_djf850.time.dt.year).mean("time").mean(axis=0)
 
-duDLMdysh_djf850 = duDLMdy.sel(time=duDLMdy.time.dt.season=="DJF")
+duDLMdysh_djf850 = duDLMdy.sel(time=duDLMdy.time.dt.month.isin([12, 1, 2, 3]))
 duDLMdysh_seasonal = duDLMdysh_djf850.groupby(duDLMdysh_djf850.time.dt.year).mean("time").mean(axis=0)
-dvDLMdxsh_djf850 = dvDLMdx.sel(time=dvDLMdx.time.dt.season=="DJF")
+dvDLMdxsh_djf850 = dvDLMdx.sel(time=dvDLMdx.time.dt.month.isin([12, 1, 2, 3]))
 dvDLMdxsh_seasonal = dvDLMdxsh_djf850.groupby(dvDLMdxsh_djf850.time.dt.year).mean("time").mean(axis=0)
 
+
+# Northern hemisphere:
+vrtDLMxnh_djf850 = vrtDLMx.sel(time=vrtDLMx.time.dt.month.isin([6, 7, 8, 9]))
+vrtDLMxnh_seasonal = vrtDLMxnh_djf850.groupby(vrtDLMxnh_djf850.time.dt.year).mean("time").mean(axis=0)
+vrtDLMynh_djf850 = vrtDLMy.sel(time=vrtDLMy.time.dt.month.isin([6, 7, 8, 9]))
+vrtDLMynh_seasonal = vrtDLMynh_djf850.groupby(vrtDLMynh_djf850.time.dt.year).mean("time").mean(axis=0)
+
+duDLMdynh_djf850 = duDLMdy.sel(time=duDLMdy.time.dt.month.isin([6, 7, 8, 9]))
+duDLMdynh_seasonal = duDLMdynh_djf850.groupby(duDLMdynh_djf850.time.dt.year).mean("time").mean(axis=0)
+dvDLMdxnh_djf850 = dvDLMdx.sel(time=dvDLMdx.time.dt.month.isin([6, 7, 8, 9]))
+dvDLMdxnh_seasonal = dvDLMdxnh_djf850.groupby(dvDLMdxnh_djf850.time.dt.year).mean("time").mean(axis=0)
 
 df = load_ibtracs_df(basins=['SP', 'SI'])
 width = 4.0
@@ -920,3 +937,39 @@ vdf = pd.DataFrame(data=np.array(
 vdf["index"] = vdf["index"].astype(int)
 outdf = df.merge(vdf, left_on="index", right_on="index", how="inner")
 outdf.to_csv("betaclim.SH.csv", index=False)
+
+
+df = load_ibtracs_df(basins=['WP'])
+width = 4.0
+
+output = []
+for row in df.itertuples():
+    dt = row.TM.to_numpy()
+    print(f"Calculating environmental flow at {dt}")
+    lat_cntr = 0.25 * np.round(row.LAT * 4)
+    lon_cntr = 0.25 * np.round(row.LON * 4)
+    lat_slice = slice(lat_cntr + width, lat_cntr - width)
+    long_slice = slice(lon_cntr - width, lon_cntr + width)
+    
+    dzdy = (
+        vrtDLMynh_seasonal.sel(lat=lat_slice, lon=long_slice).mean(
+            axis=(0, 1)).values
+    )
+    dudy = (
+        duDLMdynh_seasonal.sel(lat=lat_slice, lon=long_slice).mean(
+            axis=(0, 1)).values
+    )
+    dvdx = (
+        dvDLMdxnh_seasonal.sel(lat=lat_slice, lon=long_slice).mean(
+            axis=(0, 1)).values
+    )
+     
+    res = np.hstack((row.index, dzdy, dudy, dvdx))
+    output.append(res)
+    
+cols = ["index", "dzdy", "dudy", "dvdx"]
+vdf = pd.DataFrame(data=np.array(
+    [r for r in output]).squeeze(), columns=cols)
+vdf["index"] = vdf["index"].astype(int)
+outdf = df.merge(vdf, left_on="index", right_on="index", how="inner")
+outdf.to_csv("betaclim.WP.csv", index=False)
